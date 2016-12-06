122ba354a1a803d0424e3a98b12106c9ae20d564
juverlia <julia.vollmer06@web.de>
2016-12-06 10:06:08 +0100

    added EX50-EX52(not finished yet)

---
+++ gothonweb/tests/app_tests.py
@@ -0,0 +1,27 @@
+from nose.tools import *
+from app import app
+from tests.tools import assert_response
+
+client = app.test_client() # create a testing client (like a fake web browser)
+client.testing = True # enable this so that errors in your web app bubble up to the testing client
+
+def test_index():
+    global client
+    resp =client.get("/") # with this client you can do all kinds of stuff
+    assert_response(resp, status=302) # the root url should give back a redirect
+
+    resp = client.get("/game")
+    assert_response(resp) # valid response
+
+    resp = client.post('/game') #POST, but provide no data
+    assert_response(resp, contains="You died!")
+
+    #Go to another scene
+    testdata = {'userinput': 'tell a joke'}
+    resp = client.post('/game', data=testdata)
+    assert_response(resp, contains='Laser Weapon Armory')
+
+    #from there go to yet another scene
+    testdata = {'userinput': '132'}
+    resp = client.post('/game', data=testdata)
+    assert_response(resp, contains='The Bridge')
