b343dbac8313d8f9e16f07aab289412e0d6810da
juverlia <julia.vollmer06@web.de>
2017-01-08 18:54:44 +0800

    .

--- EX48/tests/parser_tests.py
+++ EX48/tests/parser_tests.py
@@ -1,19 +1,9 @@
-## I don't really know how to do this
 
 from nose.tools import *
-from ex48 import parser
-
-def test_subj():
-    assert_equal(parse_verb(('verb', 'run'), 'run')
-    print "Running setup for automated tests!"
+from ex48.parser import *
 
 def test_sentence():
     trysen = parse_sentence([('noun', 'bear'), ('verb', 'eat'), ('stop', 'the'), ('noun', 'honey')])
-    assert_equal(trysen, [()])
-    print "Running teardown for automated tests!"
-
-def test_obj():
-    print "Running the first and only automated test!"
-
-def test_verb():
-    pass
+    assert_equal(trysen.subject, 'bear')
+    assert_equal(trysen.verb, 'eat')
+    assert_equal(trysen.object, 'honey')
